2ceb51351836cb63081fa98fac561460bd6e1475
Leo-Sun-BMSTU <wowp.samolet@gmail.com>
2021-12-13 20:02:12 +0300

    Проектное задание №2

--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,5 @@
+import random
+import data
 from django.shortcuts import render
 
 
@@ -7,22 +9,51 @@ def main_view(request):
     :param request:
     :return:
     '''
-    return render(request, 'index.html')
+    return render(request, 'index.html', context={'title': data.title,
+                                                  'subtitle': data.subtitle,
+                                                  'description': data.description,
+                                                  'departures': data.departures,
+                                                  'tours': dict(random.sample(data.tours.items(), 6))})
 
 
-def departure_view(request):
+def departure_view(request, departure: str):
     '''
     Представление для страницы направления
     :param request:
     :return:
     '''
-    return render(request, 'departure.html')
+    return render(request, 'departure.html', context={'title': data.title,
+                                                      'subtitle': data.subtitle,
+                                                      'description': data.description,
+                                                      'departures': data.departures,
+                                                      'tours': departure_filter(data.tours, departure),
+                                                      'current_departure': departure,
+                                                      'from_city': data.departures.get(departure),
+                                                      'tour_count': len(departure_filter(data.tours, departure)),
+                                                      'min_price': min([tour.get('price') for key, tour in
+                                                                    departure_filter(data.tours, departure).items()]),
+                                                      'max_price': max([tour.get('price') for key, tour in
+                                                                    departure_filter(data.tours, departure).items()]),
+                                                      })
 
 
-def tour_view(request):
+def departure_filter(input_data: dict, departure: str) -> dict:
+    '''
+    Фильтрация по направлениям
+    :param input_data: dict
+    :param departure: str - город вылета
+    :return: dict
+    '''
+    result = {key: val for key, val in input_data.items() if val.get('departure') == departure}
+    return result
+
+
+def tour_view(request, id: int):
     '''
     Представление для страницы тура
     :param request:
     :return:
     '''
-    return render(request, 'tour.html')
+    return render(request, 'tour.html', context={'choisen_tour': data.tours.get(id),
+                                                 'departures': data.departures,
+                                                 'title': data.title})
